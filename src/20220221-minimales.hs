5856a9c39c69f5f9a70e056dac147fce16534d8d
chemacortes <devxtrem@gmail.com>
2022-02-22 00:55:29 +0100

    first commit

---
+++ src/20220221-minimales.hs
@@ -0,0 +1,25 @@
+{-
+Definir la función
+
+   minimales :: Ord a => [[a]] -> [[a]]
+
+tal que (minimales xss) es la lista de los elementos de xss que no están contenidos en otros elementos de xss. Por ejemplo,
+
+   minimales [[1,3],[2,3,1],[3,2,5]]        ==  [[2,3,1],[3,2,5]]
+   minimales [[1,3],[2,3,1],[3,2,5],[3,1]]  ==  [[2,3,1],[3,2,5]]
+   map sum (minimales [[1..n] | n <- [1..300]])  ==  [45150]
+
+-}
+
+module Minimales where
+
+import Data.List ((\\))
+
+minimales :: [[Int]] -> [[Int]]
+minimales [] = []
+minimales (xs : xss)
+  | any (xs ⊆) xss = minimales xss
+  | otherwise = xs : minimales (filter (⊈ xs) xss)
+  where
+    xs ⊆ ys = null $ xs \\ ys
+    xs ⊈ ys = not . null $ xs \\ ys
