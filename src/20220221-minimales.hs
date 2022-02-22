25e6df8ce23fd65a5db8abb8048353b130ac97e4
jmcortesa <jmcortesa@aragon.es>
2022-02-22 13:36:17 +0100

    Añadida URL del ejercicio

--- src/20220221-minimales.hs
+++ src/20220221-minimales.hs
@@ -9,6 +9,8 @@ tal que (minimales xss) es la lista de los elementos de xss que no están conten
    minimales [[1,3],[2,3,1],[3,2,5],[3,1]]  ==  [[2,3,1],[3,2,5]]
    map sum (minimales [[1..n] | n <- [1..300]])  ==  [45150]
 
+https://www.glc.us.es/~jalonso/exercitium/determinacion-de-los-elementos-minimales/
+
 -}
 
 module Minimales where
